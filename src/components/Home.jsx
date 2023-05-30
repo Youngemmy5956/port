1ef9d09ff1f804d7d85fbb959d7a2a1c1e090ca4
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-05-30 03:03:27 +0100

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Logo from "../Assets/logo.svg"
+import Logo from "../Assets/logo.svg";
+import image from "../Assets/image-web-3-desktop.jpg";
 
 export default function Home() {
 
     let links = [
-        { name: "About Me", link: "/#about" },
-        { name: "Services", link: "/#services" },
-        { name: "Pricing", link: "/#pricing" },
-        { name: "Projects", link: "/" },
-        { name: "Blog", link: "/" },
-        { name: "Contact Me", link: "/#contact" },
+        { name: "Home", link: "/#about" },
+        { name: "New", link: "/#services" },
+        { name: "Popular", link: "/#pricing" },
+        { name: "Trending", link: "/" },
+        { name: "Categories", link: "/" },
       ];
 
   return (
@@ -21,7 +21,7 @@ export default function Home() {
           <nav className="sticky top-0 py-2 z-50 ">
             <ul className="md:flex pl-9 md:pl-0 flex gap-8 z-50">
               {links.map((link) => (
-                <li className="font-semibold text-white text-[22px] leading-[29px] my-7 md:my-0 z-50 md:ml">
+                <li className="font-semibold text-black  text-[22px] leading-[29px] my-7 md:my-0 z-50 md:ml">
                   <a href={link.link}>{link.name}</a>
                 </li>
               ))}
@@ -30,6 +30,23 @@ export default function Home() {
               </button>
             </ul>
           </nav>
+        </div>
+
+        <div>
+            <span>
+                <img src={image} alt="" />
+            </span>
+            <main className='flex gap-10'>
+            <span>
+                <h1 className='text-7xl font-bold leading-2'>The Bright <br /> Future of <br /> Web 3.0?</h1>
+                
+            </span>
+
+            <span>
+                <h1 className='text-2xl font-normal leading-10'>We dive into the next evolution of the web that <br /> claims to put the power of the platforms back <br /> into the hands of the people. But is it really <br /> fulfilling its promise?</h1>
+
+            </span>
+            </main>
         </div>
 
 
