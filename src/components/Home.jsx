ff671f3d89f498b03982634c3084668b6d2577c4
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-05-30 04:45:31 +0100

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Logo from "../Assets/logo.svg";
 import image from "../Assets/image-web-3-desktop.jpg";
+import retro from "../Assets/image-retro-pcs.jpg";
+import laptos from "../Assets/image-top-laptops.jpg";
+import growth from "../Assets/image-gaming-growth.jpg";
 
 export default function Home() {
 
@@ -15,7 +18,7 @@ export default function Home() {
   return (
     <div>
 
-<div className="md:px-10 py-4 px-7 flex justify-between items-center sticky top-0 z-50">
+<div className="md:px-6 py-4 px-7 flex justify-between items-center sticky top-0 z-50">
           <img src={Logo} alt="" className="mx-36" />
 
           <nav className="sticky top-0 py-2 z-50 ">
@@ -37,16 +40,16 @@ export default function Home() {
             <span >
                 <img src={image} alt="" />
             </span>
-            <main className='flex gap-44 mt-10'>
+            <main className='flex gap-44 mt-6'>
             <span>
-                <h1 className='text-7xl font-bold leading-2'>The Bright <br /> Future of <br /> Web 3.0?</h1>
+                <h1 className='text-7xl font-bold'>The Bright <br /> Future of <br /> Web 3.0?</h1>
                 
             </span>
 
             <span>
-                <h1 className='text-2xl font-normal leading-10'>We dive into the next evolution of the web that <br /> claims to put the power of the platforms back <br /> into the hands of the people. But is it really <br /> fulfilling its promise?</h1>
+                <h1 className='text-2xl font-normal '>We dive into the next evolution of the web that <br /> claims to put the power of the platforms back <br /> into the hands of the people. But is it really <br /> fulfilling its promise?</h1>
 
-                <button className='mt-10 border-2 border-black px-10 text-3xl font-normal py-2'>Read More</button>
+                <button className='mt-6 border-2 border-black px-6 text-3xl font-semibold py-2'>READ MORE</button>
 
             </span>
             </main>
@@ -54,7 +57,7 @@ export default function Home() {
 
             <div className='w-1/4 border-2 border-black px-6 '>
                 <span className=''>
-                    <h1 className='mt-10 text-4xl font-bold mb-2'>New</h1>
+                    <h1 className='mt-6 text-4xl font-bold mb-2'>New</h1>
                     <h1 className='mb-2 font-bold text-lg'>Hydrogen VS Electric Cars</h1>
                     <h1 className='mb-4 text-[13px]'> Will hydrogen-fueled cars ever catch up to EVs?</h1>
 
@@ -98,6 +101,35 @@ export default function Home() {
 
                
             </div>
+        </div>
+
+        <div className='flex mt-28 ml-48 mb-10'>
+            <span className='flex gap-6'>
+                <img src={retro} alt="" className='h-[60%]'/>
+                <span className=''>
+                <h1 className='text-3xl mb-4 px-4'>01</h1>
+                <h1 className='text-xl px-4 font-bold'>Reviving Retro PCs</h1>
+                <h1 className='text-xl px-4'>What happens when old PCs <br /> are given modern upgrades?</h1>
+                </span>
+            </span>
+
+            <span className='flex gap-6'>
+                <img src={laptos} alt="" className='h-[60%]'/>
+                <span>
+                <h1 className='text-3xl mb-4 px-4'>02</h1>
+                <h1 className='text-xl px-4 font-bold'>Top 10 Laptops of 2022</h1>
+                <h1 className='text-xl px-4'>What happens when old PCs <br /> are given modern upgrades?</h1>
+                </span>
+            </span>
+
+            <span className='flex gap-6'>
+                <img src={growth} alt="" className='h-[60%]'/>
+                <span>
+                <h1 className='text-3xl mb-4 px-4'>03</h1>
+                <h1 className='text-xl px-4 font-bold'>The Growth of Gaming</h1>
+                <h1 className='text-xl px-4'>What happens when old PCs <br /> are given modern upgrades?</h1>
+                </span>
+            </span>
         </div>
 
 
